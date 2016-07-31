d594383135bcac6da2ce322d38bebd65128238ed
dwdonline <pd@dwdonline.com>
2016-07-31 15:38:08 -0700

    Update lemp_wp_16.sh

--- lemp_wp_16.sh
+++ lemp_wp_16.sh
@@ -48,7 +48,7 @@ add-apt-repository ppa:nginx/$nginx
 
 apt-get -y update
 
-apt-get -y install php7.0-fpm php7.0-mcrypt php7.0-curl php7.0-cli php7.0-mysql php7.0-gd php7.0-intl php7.0-xsl libperl-dev libpcre3 libpcre3-dev libssl-dev php7.0-gd libgd2-xpm-dev libgeoip-dev libgd2-xpm-dev nginx
+apt-get -y install php7.0-fpm php7.0-mcrypt php7.0-curl php7.0-cli php7.0-mysql php7.0-gd php7.0-intl php7.0-xsl libperl-dev libpcre3 libpcre3-dev libssl-dev php7.0-gd libgd2-xpm-dev libgeoip-dev libgd2-xpm-dev libssh2-1 php-ssh2 nginx
 
 echo "---> NOW, LET'S COMPILE NGINX WITH PAGESPEED"
 pause
